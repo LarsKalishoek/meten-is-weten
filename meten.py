479bb2abea2ebdc17356e922d3180f96f958584d
arsKalishoek <99069312@mydavinci.nl>
2021-09-09 08:54:06 +0200

    min en max gemaakt

--- meten.py
+++ meten.py
@@ -1,8 +1,10 @@
 a = input('kies een getal a : ')
+max = a
 b = input('kies een getal b : ')
-if a > b :
+min = b
+if a < b :
     print('a is het grootste getal')
-elif a < b :
+elif a > b :
     print('b is groter dan a')
 else:
     print('a en b zijn even groot')
